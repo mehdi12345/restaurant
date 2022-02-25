2bb00eef74caa384b308f5bda7632edab804410d
mehdi12345 <mansourmehdi62@gmail.com>
2022-02-25 18:25:17 +0100

    new widgets

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant/utils/dimensions.dart';
+import 'package:restaurant/widgets/big_text.dart';
 
 class RecommenedFoodDetail extends StatelessWidget {
   const RecommenedFoodDetail({Key? key}) : super(key: key);
@@ -9,6 +11,19 @@ class RecommenedFoodDetail extends StatelessWidget {
         body: CustomScrollView(
       slivers: [
         SliverAppBar(
+          bottom: PreferredSize(
+              preferredSize: Size.fromHeight(20),
+              child: Container(
+                color: Colors.white,
+                child: BigText(
+                  text: "Sliver app bar",
+                  size: Dimensions.font26,
+                ),
+                width: double.maxFinite,
+                padding: EdgeInsets.only(top: 5, bottom: 10),
+              )),
+          pinned: true,
+          backgroundColor: Colors.yellowAccent,
           expandedHeight: 300.0,
           flexibleSpace: FlexibleSpaceBar(
             background: Image.asset(
@@ -19,8 +34,8 @@ class RecommenedFoodDetail extends StatelessWidget {
           ),
         ),
         SliverToBoxAdapter(
-          child:
-              Text("Chiken marinated in a spiced yoghurt is placed in a large"),
+          child: Text(
+              "Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large aced in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large aced in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large aced in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large Chiken marinated in a spiced yoghurt is placed in a large"),
         )
       ],
     ));
